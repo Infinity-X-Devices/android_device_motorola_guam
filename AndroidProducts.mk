93c5959741ed845b19495c0d040bb28e591f07d5
MD Raza <delightreza@gmail.com>
2024-06-14 08:13:51 +0530

    guam: Adapt For ProjectInfinity-X
    Change-Id: I5cae37e6371e051cc1ee42d710b4d594d2318569

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_guam.mk
+    $(LOCAL_DIR)/infinity_guam.mk
--- device.mk
+++ device.mk
@@ -27,8 +27,7 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Rootdir
 PRODUCT_PACKAGES += \
